2594e939b433b2045ba2a32888d49e1ce57afd97
Tadela Balakrishna <thadelabalakrishna8@gmail.com>
2025-02-10 10:21:43 +0530

    h

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask,render_template,request
+from test import TextToNum
+import pickle
+
 app=Flask(__name__)
 
 @app.route('/')
@@ -9,7 +12,23 @@ def predict():
     if request.method=="POST":
         msg=request.form.get("message")
         print(msg)
-    else:
+        ob=TextToNum(msg)
+        ob.cleaner()
+        ob.token()
+        ob.removeStop()
+        st=ob.stemme()
+        stem_vector=" ".join(st)
+
+        with open("vectorizer.pickle","rb") as vc:
+            vectorizer=pickle.load(vc)
+        vcdata=vectorizer.transform([stem_vector]).toarray()
+        print(vcdata)
+        
+        with open("model.pickle","rb") as mc:
+            model=pickle.load(mc)
+
+        pred=model.predict(vcdata)
+        print(pred)
         return render_template("predict.html")
-if __name__=="__main__":
-    app.run(host="0.0.0.0",port=5050)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5050)
